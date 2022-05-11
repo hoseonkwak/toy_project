374b8346ca3fafb7e674eb321cd963a61df8c996
kwak2864 <kwak2864@emoney.co.kr>
2022-05-11 09:58:22 +0900

    edit toy3

--- common/js/main.js
+++ common/js/main.js
@@ -1,7 +1,20 @@
 window.addEventListener('DOMContentLoaded', function() {
+
+  //swiper
+  new Swiper('.wrap .swiper', {
+    autoplay: false,
+    loop: false,
+  });
+
+  manageOpenClose(1);
+  manageOpenClose(2);
+  manageOpenClose(3);
+
   //json 데이터
   const jsonUrl = 'https://raw.githubusercontent.com/hoseonkwak/toy_project/master/bank.json';
-  //const jsonUrl = '../bank.json';
+  const jsonUrl2 = 'https://raw.githubusercontent.com/hoseonkwak/toy_project/master/bank2.json';
+  const jsonUrl3 = 'https://raw.githubusercontent.com/hoseonkwak/toy_project/master/bank3.json';
+  // const jsonUrl = '../bank.json';
   const dataResult = fetch(jsonUrl)
     .then((res) => {
       return res.json();
@@ -9,22 +22,53 @@ window.addEventListener('DOMContentLoaded', function() {
     .then((obj) => {
       //console.log(obj.bankList[0]);
       const date = new Date();
-      main_money(obj);  // 잔고
-      progressbar(obj, date); // 계좌 그래프, 남은 금액
-      accountHistory(obj, date);  // 계좌내역
-      dateChart(obj, date); // 일간 그래프
-      outPattern(obj, date);  //지출패턴
+      main_money(obj, 1);  // 잔고
+      progressbar(obj, date, 1); // 계좌 그래프, 남은 금액
+      accountHistory(obj, date, 1);  // 계좌내역
+      dateChart(obj, date, 1); // 일간 그래프
+      outPattern(obj, date, 1);  //지출패턴
+      console.log(obj.bankList);
+    });
+
+    const dataResult2 = fetch(jsonUrl2)
+    .then((res) => {
+      return res.json();
+    })
+    .then((obj) => {
+      //console.log(obj.bankList[0]);
+      const date = new Date();
+      main_money(obj, 2);  // 잔고
+      progressbar(obj, date, 2); // 계좌 그래프, 남은 금액
+      accountHistory(obj, date, 2);  // 계좌내역
+      dateChart(obj, date, 2); // 일간 그래프
+      outPattern(obj, date, 2);  //지출패턴
+      console.log(obj.bankList);
+    });
+
+    const dataResult3 = fetch(jsonUrl3)
+    .then((res) => {
+      return res.json();
+    })
+    .then((obj) => {
+      //console.log(obj.bankList[0]);
+      const date = new Date();
+      main_money(obj, 3);  // 잔고
+      progressbar(obj, date, 3); // 계좌 그래프, 남은 금액
+      accountHistory(obj, date, 3);  // 계좌내역
+      dateChart(obj, date, 3); // 일간 그래프
+      outPattern(obj, date, 3);  //지출패턴
+
       console.log(obj.bankList);
     });
 
 
 
   // 잔고
-  function main_money(obj) {
+  function main_money(obj, num) {
     let minus = 0;  //지출
     let plus = 0; //수입
 
-    const main_money = document.querySelector('.account_balance_area span');
+    const main_money = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_balance_area span`);
 
     // 지출
     const outArray = obj.bankList.filter(function(e) {
@@ -58,28 +102,31 @@ window.addEventListener('DOMContentLoaded', function() {
     const mainhh2 = window.outerHeight;
     sethh(mainhh2);
   });
-  sethh(winhh);
+  sethh(winhh, 1);
+  sethh(winhh, 2);
+  sethh(winhh, 3);
 
-  // 지출관리 열기
-  const openManage = document.querySelector('.account_goal_area .manage_btn');
-  openManage.addEventListener('click', () => {
-    document.querySelector('.manage_wrap').style.cssText = 'opacity: 1; top: 0';
-  });
-
-  // 지출관리 닫기버튼
-  const manageCloseBtn = document.querySelector('.manage_wrap header.manage_header .close_btn');
-  manageCloseBtn.addEventListener('click', () => {
-    document.querySelector('.manage_wrap').style.cssText = 'opacity: 0; top: 100%';
-  })
+  function manageOpenClose(num){
+    // 지출관리 열기
+    const openManage = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_goal_area .manage_btn`);
+    openManage.addEventListener('click', () => {
+      document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .manage_wrap`).style.cssText = 'opacity: 1; top: 0';
+    });
+  
+    // 지출관리 닫기버튼
+    const manageCloseBtn = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .manage_wrap header.manage_header .close_btn`);
+    manageCloseBtn.addEventListener('click', () => {
+      document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .manage_wrap`).style.cssText = 'opacity: 0; top: 100%';
+    })
+  }
 
   /* 메인화면 높이 조절 */
-  function sethh(winhh){
+  function sethh(winhh, num){
     const mainHeader = document.querySelector('.main_header');
     const mainHeaderhh = mainHeader.offsetHeight;
     const accountMain = document.querySelector('.account_main');
     const accountMainhh = accountMain.offsetHeight;
-    const accountHistory =  document.querySelector('.account_history');
-    //const accountHistoryhh = accountHistory.offsetHeight;
+    const accountHistory =  document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_history`);
     const mainMenu = document.querySelector('.menu');
     const menuhh = mainMenu.offsetHeight;
     const managehh = document.querySelector('.manage_wrap');
@@ -93,7 +140,7 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   // progressbar
-  function progressbar(obj, date){
+  function progressbar(obj, date, num){
     /*
     * 전체, 목표, 쓴 금액, 디데이, 사용가능 금액, 월급
     * 현재는 이번달 지출 / 이번달 목표액 * 100
@@ -104,9 +151,9 @@ window.addEventListener('DOMContentLoaded', function() {
     const thisMonthString = String(thisMonth).padStart(2, '0'); // 이번달 문자형으로 추출
     const monthPay = 3000000; // 월급
     const limitMoney = 2000000; //이번달 limit
-    const goalBar = document.querySelector('.account_goal_area .progressbar .goal_bar');
-    const goalStick = document.querySelector('.progressbar .goal_stick');
-    const now_bar = document.querySelector('.progressbar .now_bar');
+    const goalBar = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_goal_area .progressbar .goal_bar`);
+    const goalStick = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .progressbar .goal_stick`);
+    const now_bar = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .progressbar .now_bar`);
 
     let sum = 0;  //전체
     let thisMonthOut = 0;
@@ -129,7 +176,11 @@ window.addEventListener('DOMContentLoaded', function() {
     for(let i = 0; i < thisMonthOutArr.length; i++){
       thisMonthOut += thisMonthOutArr[i].price;
     }
-    console.log(thisMonthOut);
+    //console.log(thisMonthOut);
+
+    //지출관리 이번달 지출
+    const monthTotalOut = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .spend_wrap .totalOut_area .totalOut`);
+    monthTotalOut.textContent = priceToString(thisMonthOut);
 
     // 현재 사용 %
     const nowPer = thisMonthOut / limitMoney * 100;
@@ -146,14 +197,17 @@ window.addEventListener('DOMContentLoaded', function() {
     //console.log(remainDate);
 
     // 이번달 남을 일
-    document.querySelector('.account_goal_area .term').textContent = remainDate;
+    document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_goal_area .term`).textContent = remainDate;
     // 사용 가능 금액
-    document.querySelector('.account_goal_area .pay').textContent = priceToString(limitMoney - thisMonthOut);
+    document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_goal_area .pay`).textContent = priceToString(limitMoney - thisMonthOut);
 
     //지출관리
-    const manageGoalBar = document.querySelector('.progressbar.manage .goal_bar');
-    const manageGoalStick = document.querySelector('.progressbar.manage .goal_stick');
-    const manageNow_bar = document.querySelector('.progressbar.manage .now_bar');
+    const manageGoalBar = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .progressbar.manage .goal_bar`);
+    const manageGoalStick = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .progressbar.manage .goal_stick`);
+    const manageNow_bar = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .progressbar.manage .now_bar`);
+    const manageLimit = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .goal_area .title_area span`);
+
+    manageLimit.textContent = priceToString(limitMoney);
 
     manageNow_bar.style.width = `${nowPer}%`;
     manageGoalStick.style.left = `${thisGoalMoney.toFixed(2)}%`;
@@ -161,9 +215,9 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   // account_history
-  function accountHistory(obj, date){
+  function accountHistory(obj, date, num){
     
-    const dayUl = document.querySelector('.account_history_area .day_history');
+    const dayUl = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .account_history_area .day_history`);
 
     let dateI = [];
 
@@ -258,8 +312,8 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   //일간그래프
-  function dateChart(obj, date){
-    const canvas = document.querySelector('#dateChart1');
+  function dateChart(obj, date, num){
+    const canvas = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) #dateChart1`);
 
     // 이번달 구하기
     const thisMonth = date.getMonth() +1 ;
@@ -326,12 +380,12 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   // 지출 패턴
-  function outPattern(obj, date){
-    const canvas = document.querySelector('#monthChart1');
+  function outPattern(obj, date, num){
+    const canvas = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) #monthChart1`);
 
     const thisMonth = date.getMonth() +1;
-    const monthSpan = document.querySelector('.manage_wrap .manage_scroll_area .spend_wrap h3 span');
-    console.log(thisMonth);
+    const monthSpan = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .manage_wrap .manage_scroll_area .spend_wrap h3 span`);
+    //console.log(thisMonth);
     monthSpan.textContent = thisMonth;
 
     let classifyArr = [];
@@ -340,11 +394,11 @@ window.addEventListener('DOMContentLoaded', function() {
     for(let i = 0; i < obj.bankList.length; i++){
       if(obj.bankList[i].classify !== '') classifyArr.push(obj.bankList[i].classify);
     }
-    console.log(classifyArr);
+    //console.log(classifyArr);
 
     // 겹치는 분류 제거
     const set = new Set(classifyArr);
-    console.log(set);
+    //console.log(set);
 
     
     let patternArr = [];  //패턴 값
@@ -354,14 +408,14 @@ window.addEventListener('DOMContentLoaded', function() {
       const pattern = obj.bankList.filter((e) => {
         return e.classify === [...set][i];
       });
-      console.log(pattern);
+      //console.log(pattern);
       for(let i = 0; i < pattern.length; i++){
         patternValue += pattern[i].price;
       }
-      console.log(patternValue);
+      //console.log(patternValue);
       patternArr.push(patternValue);
     }
-    console.log(patternArr);
+    //console.log(patternArr);
 
     const data = {
       labels:patternArr,
@@ -389,6 +443,36 @@ window.addEventListener('DOMContentLoaded', function() {
       },
     });
 
+    //패턴 리스트
+    for(let i = 0; i < [...set].length; i++){
+      const patternUl = document.querySelector(`.swiper .swiper-slide:nth-child(${num}) .pattern_list`);
+      
+      const patternLi = document.createElement('li');
+      const patternTitleSpan = document.createElement('span');
+      patternTitleSpan.classList.add('pattern_title');
+      const patternMoneySpan = document.createElement('span');
+      patternMoneySpan.classList.add('pattern_money');
+
+      if([...set][i] === 'health'){
+        patternTitleSpan.textContent = '건강관리비';
+      } else if([...set][i] === 'eatout'){
+        patternTitleSpan.textContent = '외식비';
+      } else if([...set][i] === 'mart'){
+        patternTitleSpan.textContent = '장보기';
+      } else if([...set][i] === 'shopping'){
+        patternTitleSpan.textContent = '상점';
+      } else if([...set][i] === 'oiling'){
+        patternTitleSpan.textContent = '주유비';
+      }
+
+      patternMoneySpan.textContent = `${priceToString(patternArr[i])}원`;
+
+      //console.log(priceToString(patternArr[i]));
+      patternLi.appendChild(patternTitleSpan);
+      patternLi.appendChild(patternMoneySpan);
+      patternUl.appendChild(patternLi);
+
+    }
   }
 
 
